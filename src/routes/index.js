e8bf97fd5880d034b1903d20c8c626a9e85e09f9
songcs <songcs0329@gmail.com>
2021-07-17 18:41:33 +0900

    210717 vue 끝장내기 프론트 작업중(회원가입)

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/LoginPage.vue'),
+  },
+  {
+    path: '/signup',
+    component: () => import('@/views/SignupPage.vue'),
+  },
+  {
+    path: '*',
+    component: () => import('@/views/NotFoundPage.vue'),
+  },
+];
+
+export default new VueRouter({
+  mode: 'history',
+  routes,
+});
