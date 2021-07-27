57b4f860bd5e1c12663d8351700ff8a942bdffb8
songcs <songcs0329@gmail.com>
2021-07-27 20:41:23 +0900

    메인페이지 학습노트 리스트 랜더링

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,12 +2,46 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading" />
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        />
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api';
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+
+export default {
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      this.isLoading = false;
+      this.postItems = data.posts;
+    },
+  },
+};
 </script>
 
 <style></style>
